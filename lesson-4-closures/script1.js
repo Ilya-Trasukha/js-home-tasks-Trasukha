8e63108693abb3a52dd1e4a3f723bf7efccbb610
Ilya Tasukha <89993599+Ilya-Trasukha@users.noreply.github.com>
2022-02-14 10:03:15 +0300

    Update script1.js

--- lesson-4-closures/script1.js
+++ lesson-4-closures/script1.js
@@ -1,10 +1,15 @@
-var size = 8;
-var board = "";
-for(var y = 0; y < size; y++) {
-	for(var x = 0; x < size; x++) {
-		if((x + y) % 2 == 0) board += " ";
-		else board += "#";
-	}
-	board += "\n";
+var board = '';
+do {
+    var size = +prompt('Введите размер решетки', '');
+} while (!size || size == '' || isNaN(size));
+for (var y = 0; y < size; y++) {
+  for (var x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) {
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
 }
 console.log(board);
